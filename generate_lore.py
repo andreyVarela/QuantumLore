5d4912e38430dcd892121692f2a51c0e7ef14b9f
Andrei Varela <avsolano95@gmail.com>
2025-07-08 22:51:40 -0600

    new prompt and save .txt

--- generate_lore.py
+++ generate_lore.py
@@ -1,10 +1,13 @@
 from google import genai
+import os
+from cloudflare import Cloudflare
+import random
 
 #! API KEY (pasar a .env luego)
 client = genai.Client(api_key="AIzaSyDYsZkqjhp96mAbRYTZYSJ1yJk_DF4ngvE")
 model = "gemini-2.5-flash"
-
-def generate_lore(universo):
+acount_id = "36a986f20bee29ef1a6c96b304a202c1"
+def generate_lore(nombre_universo, tematica, num_personajes, num_facciones, num_misiones, num_ubicaciones):
 
     prompt = f"""
     Eres un generador experto de lore para juegos de fantasía y ciencia ficción.
@@ -45,7 +48,40 @@ def generate_lore(universo):
         model=model,
         contents=prompt
     )
-    print(response)
-    return response.text
+    #image = generate_lore_image(response.text)
 
-print(generate_lore("2077 Planet city"))
+    return response.text#, image
+
+def generate_lore_image(prompt):
+    prompt = f"""
+    Crea una imagen que represente esta historia: {prompt}
+    """
+
+    client = Cloudflare(
+    api_token=os.environ.get("g1u0rdzN4oisEmlYuJZ6kUt0xgZdAw9JaapYbFLw"),  # This is the default and can be omitted
+)
+    response = client.ai.run(
+        model_name="model_name",
+        account_id=acount_id,
+        text=prompt,
+    )
+    return response.image_url
+
+def main():
+    nombre_universo=str(input("Introduce el nombre del universo: "))
+    tematica = str(input("Introduce la temática del universo (fantasía, ciencia ficción, etc.): "))
+    num_personajes = random.randint(5, 10)
+    num_facciones = random.randint(3, 5)
+    num_misiones = random.randint(2, 4)
+    num_ubicaciones = random.randint(3, 6)
+
+    lore = generate_lore(nombre_universo, tematica, num_personajes, num_facciones, num_misiones, num_ubicaciones)
+        # Guardar en archivo de texto
+    filename = f"{nombre_universo.replace(' ', '_').lower()}_lore.txt"
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(lore)
+    
+    print(f"\n✅ Historia guardada en '{filename}'")
+
+if __name__ == "__main__":
+    main()
